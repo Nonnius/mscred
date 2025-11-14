e4693e73223bbc2384a9bde607147d137b4e62d2
Nonnius <omarr.reid@outlook.com>
2025-11-14 06:40:05 -0500

    Update deployed app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,10 +56,10 @@ const Navbar = () => {
                         <a href="https://reid-education.vercel.app" 
                         onClick={() => setShowMobileMenu(false)}
                         className="block text-[24px] cursor-pointer text-cyan-500 font-semibold">Education</a>
-                        <a href="https://project-gallery.vercel.app" 
+                        <a href="https://project-gallery-blush.vercel.app" 
                         onClick={() => setShowMobileMenu(false)}
                         className="block text-[24px] cursor-pointer text-cyan-500 font-semibold">Projects Gallery</a>                
-                        <a href="https://project-gallery-blush.vercel.app/#contact" 
+                        <a href="https://reid-dev-portfolio.vercel.app/#contact" 
                         onClick={() => setShowMobileMenu(false)}
                         className="block text-[24px] cursor-pointer text-cyan-500 font-semibold">Contact</a>
                 </nav>
